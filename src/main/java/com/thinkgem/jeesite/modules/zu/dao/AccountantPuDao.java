033e695f9982e60d9b252e21c43234ea2a2ef9c3
nilm <niliangming@163.com>
2017-10-30 18:01:42 +0800

    feat：树谱相关

---
+++ src/main/java/com/thinkgem/jeesite/modules/zu/dao/AccountantPuDao.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright &copy; 2012-2016 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package com.thinkgem.jeesite.modules.zu.dao;
+
+import com.thinkgem.jeesite.common.persistence.TreeDao;
+import com.thinkgem.jeesite.common.persistence.annotation.MyBatisDao;
+import com.thinkgem.jeesite.modules.zu.entity.AccountantPu;
+
+/**
+ * 族谱DAO接口
+ * @author 倪得渊
+ * @version 2017-10-06
+ */
+@MyBatisDao
+public interface AccountantPuDao extends TreeDao<AccountantPu> {
+	
+}
